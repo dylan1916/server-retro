fb0f4005c19e53a0e08a506485e29c338125b2c1
Tordjman <dylantordjman@MacBook-Air-de-Tordjman.local>
2019-09-25 18:49:56 +0200

    first commit

---
+++ apiRouter.js
@@ -0,0 +1,16 @@
+// Imports
+var express = require('express');
+var usersCtrl = require('./routes/usersCtrl');
+
+// Routes
+exports.router = (function () {
+    var apiRouter = express.Router();
+
+    //Users routes
+    apiRouter.route('/users/register/').post(usersCtrl.register);
+    apiRouter.route('/users/login/').post(usersCtrl.login);
+    apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
+    apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+
+    return apiRouter;
+})();
---
+++ utils/jwt.utils.js
@@ -0,0 +1,35 @@
+// Imports
+var jwt = require('jsonwebtoken');
+
+const JWT_SIGN_SECRET = 'hyehd7737y3ehndjnc02874hjncdjnjnakmcjhqrajsjd7364egd6gd46gw17hwu28kigfs076t6jc';
+
+// Exported functions
+module.exports = {
+    generateTokenForUser: function(userData){
+        return jwt.sign({
+            userId: userData.id,
+            isAdmin: userData.isAdmin
+        },
+        JWT_SIGN_SECRET,
+        {
+            expiresIn: '1h'
+        })
+    },
+
+    parseAuthorization: function(authorization) {
+        return (authorization != null) ? authorization.replace('Bearer ', '') : null;
+    },
+
+    getUserId: function(authorization) {
+        var userId = -1;
+        var token = module.exports.parseAuthorization(authorization);
+        if (token != null) {
+            try {
+                var jwtToken = jwt.verify(token, JWT_SIGN_SECRET);
+                if (jwtToken != null)
+                    userId = jwtToken.userId;
+            } catch(err) {}
+        }
+        return userId;
+    }
+}
